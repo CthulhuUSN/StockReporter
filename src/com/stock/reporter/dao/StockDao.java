e7d2d58c30a788f010839e49f4fd89acba2a7c19
rajnaidu <l.rajen@hotmail.com>
2019-03-17 15:40:30 -0400

    Database connection

--- src/com/stock/reporter/dao/StockDao.java
+++ src/com/stock/reporter/dao/StockDao.java
@@ -1,9 +1,51 @@
 package com.stock.reporter.dao;
 
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Data implementation
  * @date Mar 17, 2019
  */
-public class StockDao {
+public class StockDao implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	private static Connection connect = null;
+	private static StockDao instance = null;
+	
+	/**
+	 * Initialize database connection
+	 */
+	private StockDao() {
+		try {
+            String url = "jdbc:sqlite:stockreporter.db";
+            connect = DriverManager.getConnection(url);            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
+	
+	/**
+	 * get database connection instance
+	 * @return db connection instance
+	 */
+	public static StockDao getInstance() {
+		if(instance == null) {
+			instance = new StockDao();
+		}
+		return instance;
+	}
+	
+    private void disconnect(){
+        try {
+            if (connect != null) {
+            	connect.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
