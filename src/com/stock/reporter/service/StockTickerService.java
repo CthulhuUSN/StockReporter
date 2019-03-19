3f6ce9f9d68e0b7c8cb8c3b2acaaac37c6f40345
rajnaidu <l.rajen@hotmail.com>
2019-03-18 20:05:24 -0400

    Comments added...

--- src/com/stock/reporter/service/StockTickerService.java
+++ src/com/stock/reporter/service/StockTickerService.java
@@ -11,13 +11,29 @@ import com.stock.reporter.domain.StockTicker;
 public class StockTickerService {
 	StockDao stockDao = null;
 	
+	/**
+	 * Initialize dao object
+	 */
 	public StockTickerService() {
 		stockDao = new StockDao();
 	}
 	
+	/**
+	 * Transaction enabled
+	 * @param stockTicker
+	 * @return
+	 */
 	public int insert(StockTicker stockTicker) {
 		return insert(stockTicker, true);
 	}
+	
+	/**
+	 * Transaction flag is set
+	 * Will be useful for running unit test cases with transaction off
+	 * @param stockTicker
+	 * @param isTransactional
+	 * @return
+	 */
 	public int insert(StockTicker stockTicker, boolean isTransactional) {
 		
 		System.out.println("Inside StockTickerService -> insert");
