6423e0d238fd4005d2b252d7703a64ef7df55a09
rajnaidu <l.rajen@hotmail.com>
2019-03-27 20:56:30 -0400

    Database connection for prod and dev

---
+++ StockAnalysisTool/src/com/stock/reporter/dao/DBConnect.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stock.reporter.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * Manage production database connection
+ */
+public class DBConnect {
+    private static Connection connect = null;
+
+	/**
+	 * Establish database connection
+	 */
+	static {
+            try {
+                String url = "jdbc:sqlite:stockreporter.prod";
+                connect = DriverManager.getConnection(url);  
+                System.out.println("Connection to SQLite has been established.");
+            } catch(SQLException ex) {
+        	System.out.println(ex.getMessage());
+            }
+	}
+	
+	/**
+	 * private constructor to prevent class instance.
+	 */
+	private DBConnect() {
+		
+	}
+	
+	/**
+	 * get database connection instance
+	 * @return connection instance
+	 */
+	public static Connection getInstance() {
+		 return connect;
+	}
+	
+	/**
+	 * Disconnect the connection
+	 */
+    public static void disconnect(){
+        try {
+            if (connect != null) {
+            	connect.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
