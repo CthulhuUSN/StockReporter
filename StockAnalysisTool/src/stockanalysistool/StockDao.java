a385f58321ad6c5f17b37010df79858621adc2af
Herve Tchoufong <hervet@Herves-MacBook-Pro-3.local>
2019-03-29 13:59:17 -0400

    Created Utility class and cleaned up references to Historical data

--- StockAnalysisTool/src/stockanalysistool/StockDao.java
+++ StockAnalysisTool/src/stockanalysistool/StockDao.java
@@ -176,46 +176,43 @@ public final class StockDao {
         }
     }
     
-    public void insertStockHistoricalData(StockHistorical stockHistorical){
+    public void insertStockSummaryData(StockSummary stockSummary){
         //Commenting this out until we get to the historical data part of the project. -Jason
-        /*
-        String sql = "INSERT INTO "+Constants.TABLE_STOCKS+" ("
-                + Constants.FIELD_SYMBOL + ","
-                + Constants.FIELD_SOURCE + ","
-                + Constants.FIELD_DATE + ","
-                + Constants.FIELD_OPEN + ","
-                + Constants.FIELD_HIGH + ","
-                + Constants.FIELD_LOW + ","
-                + Constants.FIELD_CLOSE + ","
-                + Constants.FIELD_ADJUSTED_CLOSE + ","
-                + Constants.FIELD_VOLUME + ")"
-                + " values (?,?,?,?,?,?,?,?,?)";
-        try{
-            connect(); //<--CONNECTS TO DATABASE BEFORE STARTING AN OPERATION
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, stockHistorical.getSymbol());
-            pstmt.setString(2, stockHistorical.getSource());
-            pstmt.setDate(3, stockHistorical.getDate());
-            pstmt.setInt(4, stockHistorical.getOpen());
-            pstmt.setInt(5, stockHistorical.getHigh());
-            pstmt.setInt(6, stockHistorical.getLow());
-            pstmt.setInt(7, stockHistorical.getClose());
-            pstmt.setInt(8, stockHistorical.getAdjClose());
-            pstmt.setInt(9, stockHistorical.getVolume());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            disconnect(); //<--DISCONNECTS FROM DATABASE AFTER COMPLETING THE OPERATION
-        }
-        */
+        
+        //I renamed this from insertStockHistoricalData to insertStockSummaryData. 
+        //But it needs to be implemented to match the StockSummary class fields - Herve
+        
+//        String sql = "INSERT INTO "+Constants.TABLE_STOCKS+" ("
+//                + Constants.FIELD_SYMBOL + ","
+//                + Constants.FIELD_SOURCE + ","
+//                + Constants.FIELD_DATE + ","
+//                + Constants.FIELD_OPEN + ","
+//                + Constants.FIELD_HIGH + ","
+//                + Constants.FIELD_LOW + ","
+//                + Constants.FIELD_CLOSE + ","
+//                + Constants.FIELD_ADJUSTED_CLOSE + ","
+//                + Constants.FIELD_VOLUME + ")"
+//                + " values (?,?,?,?,?,?,?,?,?)";
+//        try{
+//            connect(); //<--CONNECTS TO DATABASE BEFORE STARTING AN OPERATION
+//            PreparedStatement pstmt = conn.prepareStatement(sql);
+////            pstmt.setString(1, stockSummary.getSymbol());
+////            pstmt.setString(2, stockSummary.getSource());
+////            pstmt.setDate(3, stockSummary.getDate());
+//            pstmt.setInt(4, stockSummary.getOpen());
+//            pstmt.setInt(5, stockSummary.getHigh());
+//            pstmt.setInt(6, stockSummary.getLow());
+//            pstmt.setInt(7, stockSummary.getClose());
+//            pstmt.setInt(8, stockSummary.getAdjClose());
+//            pstmt.setInt(9, stockSummary.getVolume());
+//            pstmt.executeUpdate();
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        } finally {
+//            disconnect();
+//        }
+        
     }
-    
-    public void insertStockSummaryData(StockSummary stockSummary){}
-    
-    public void updateStockHistoricalData(StockHistorical stockHistorical){}
-    
-    public void updateStockSummaryData(StockSummary stockSummary){}
     
     public void getAvgStockSummaryView(){
         String sql = "SELECT * FROM STOCK_SUMMARY_VIEW;";
@@ -233,45 +230,6 @@ public final class StockDao {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-    }
-    
-    public void getAvgStockHistoricalView(){
-    //Moved this here from selectAll() to keep the class organized and to keep the code - Jason
-    //Commenting this out until we get to the historical data part of the project -Jason
-    /*
-        String sql = "SELECT "+Constants.FIELD_ID+", "
-                +Constants.FIELD_SYMBOL+", "
-                +Constants.FIELD_SOURCE+", "
-                +Constants.FIELD_DATE+", "
-                +Constants.FIELD_OPEN+", "
-                +Constants.FIELD_HIGH+", "
-                +Constants.FIELD_LOW+", "
-                +Constants.FIELD_CLOSE+", "
-                +Constants.FIELD_ADJUSTED_CLOSE+", "
-                +Constants.FIELD_VOLUME
-                +" FROM "+Constants.TABLE_STOCKS;
-        try{
-            connect();
-            Statement stmt  = conn.createStatement();
-            ResultSet rs    = stmt.executeQuery(sql);
-            while (rs.next()) {
-                System.out.println(rs.getInt(Constants.FIELD_ID) + "\t" 
-                        +  rs.getString(Constants.FIELD_SYMBOL)  + "\t" 
-                        +  rs.getString(Constants.FIELD_DATE)  + "\t" 
-                        +  rs.getString(Constants.FIELD_OPEN)  + "\t" 
-                        +  rs.getString(Constants.FIELD_HIGH)  + "\t" 
-                        +  rs.getString(Constants.FIELD_LOW)  + "\t" 
-                        +  rs.getString(Constants.FIELD_CLOSE)  + "\t" 
-                        +  rs.getString(Constants.FIELD_ADJUSTED_CLOSE)  + "\t" 
-                        +  rs.getString(Constants.FIELD_VOLUME)
-                );
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            disconnect();
-        }
-    */
     }
     
     public void deleteAll(){
