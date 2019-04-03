e662f34dcec02d260af43efcc5c647900781b52d
jasonarvidson <jasonarvidson@jasons-mbp.domain>
2019-04-03 18:05:30 -0500

    Finished the testing classes for the scrapper classes.

---
+++ StockReporter/test/stockreporter/scrappers/YahooScraperTest.java
@@ -0,0 +1,15 @@
+package stockreporter.scrappers;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class YahooScraperTest {
+    
+    public YahooScraperTest() {
+    }
+
+    @Test
+    public void testScapeSingleSummaryData() {
+    }
+    
+}
