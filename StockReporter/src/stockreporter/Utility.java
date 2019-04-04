55157cc0dd3bdf5c28b04f0fe22747b744a58393
rajnaidu <l.rajen@hotmail.com>
2019-04-04 10:06:54 -0400

    Class and method level comments

--- StockReporter/src/stockreporter/Utility.java
+++ StockReporter/src/stockreporter/Utility.java
@@ -9,10 +9,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author Herve Tchoufong
+ * Utility class for conversion and generic methods
  */
 public class Utility {
+    
+    /**
+     * Convert string value to big decimal
+     * @param stringCurrency
+     * @return 
+     */
     public static BigDecimal convertStringCurrency(String stringCurrency){
         double doubleCurrency = 0;
         if (Character.isLetter(stringCurrency.charAt(stringCurrency.length()-1))){
@@ -27,6 +32,11 @@ public class Utility {
         return BigDecimal.valueOf(doubleCurrency);
     }
     
+    /**
+     * Get thousands by B/M
+     * @param letter
+     * @return 
+     */
     public static double getThousands(char letter){
         if (letter == 'M')
             return 1000000;
@@ -35,6 +45,11 @@ public class Utility {
         return 1;
     }
     
+    /**
+     * Convert currency from String to Double
+     * @param stringCurrency
+     * @return 
+     */
     public static double getDoubleCurrency(String stringCurrency){
         try {
             return NumberFormat.getNumberInstance(java.util.Locale.US).parse(stringCurrency).doubleValue();
@@ -44,13 +59,28 @@ public class Utility {
         return 1;
     }
     
+    /**
+     * Get min/max range
+     * @param range
+     * @return 
+     */
     public static String[] getRangeMinAndMax(String range){
         return range.split("-");
     }
+    /**
+     * Get numerator and denominator for fraction
+     * @param fraction
+     * @return 
+     */
     public static String[] getNumeratorAndDenominator(String fraction){
         return fraction.trim().equals("-")?new String[]{"0","0"}:fraction.split("/");
     }
     
+    /**
+     * Compute the string values
+     * @param str
+     * @return 
+     */
     public static String computeStringValues(String str){
         String[] operands = str.split("x");
         double operand1 = convertStringCurrency(operands[0]).doubleValue(); 
@@ -59,10 +89,20 @@ public class Utility {
         return String.valueOf(result);
     }
     
+    /**
+     * Get the number before parantheses for a given string
+     * @param str
+     * @return 
+     */
     public static String getNumberBeforeParantheses(String str){
         return str.trim().equals("-")?"0":(str.split("\\(")[0]).trim();
     }
     
+    /**
+     * Format date string
+     * @param dateString
+     * @return 
+     */
     public static String formatDateString(String dateString){
         try {
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -74,6 +114,11 @@ public class Utility {
         return dateString;
     }
     
+    /**
+     * check if the data is blank
+     * @param str
+     * @return 
+     */
     public static boolean isBlank(String str){
         return str.equals("-") || str.equals("N/A");
     }
