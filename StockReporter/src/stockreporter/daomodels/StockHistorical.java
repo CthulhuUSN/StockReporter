36394714892d8b097e3fd86e27711577257636d5
rajnaidu <l.rajen@hotmail.com>
2019-04-04 10:13:37 -0400

    Removed attributes, setter/getter from StockHistorical <TODO> in next phase of the project

--- StockReporter/src/stockreporter/daomodels/StockHistorical.java
+++ StockReporter/src/stockreporter/daomodels/StockHistorical.java
@@ -6,72 +6,7 @@
 package stockreporter.daomodels;
 
 /**
- *
- * @author Herve Tchoufong
+ * Entity object for stock historical data
+ * TODO in next phase of the project
  */
-public class StockHistorical {
-    private int historicalId;
-    private int open;
-    private int high;
-    private int low;
-    private int close;
-    private int adjClose;
-    private int volume;
-
-    public int getHistoricalId() {
-        return historicalId;
-    }
-
-    public void setHistoricalId(int historicalId) {
-        this.historicalId = historicalId;
-    }
-
-    public int getOpen() {
-        return open;
-    }
-
-    public void setOpen(int open) {
-        this.open = open;
-    }
-
-    public int getHigh() {
-        return high;
-    }
-
-    public void setHigh(int high) {
-        this.high = high;
-    }
-
-    public int getLow() {
-        return low;
-    }
-
-    public void setLow(int low) {
-        this.low = low;
-    }
-
-    public int getClose() {
-        return close;
-    }
-
-    public void setClose(int close) {
-        this.close = close;
-    }
-
-    public int getAdjClose() {
-        return adjClose;
-    }
-
-    public void setAdjClose(int adjClose) {
-        this.adjClose = adjClose;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-    
-}
+public class StockHistorical {}
