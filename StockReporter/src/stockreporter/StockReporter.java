38027bfbf9f653d826eea208efb510e5b1fe4e9d
rajnaidu <l.rajen@hotmail.com>
2019-04-04 09:56:49 -0400

    Class and method level comments

--- StockReporter/src/stockreporter/StockReporter.java
+++ StockReporter/src/stockreporter/StockReporter.java
@@ -3,15 +3,20 @@ package stockreporter;
 import stockreporter.scrappers.YahooScraper;
 import stockreporter.scrappers.InvestopediaScraper;
 
+/**
+ * Main class for scrapping the data
+ */
 public class StockReporter {
     
     public static void main(String[] args) {    
-        //create the database oject
+        //create the database instance
         StockDao dao = StockDao.getInstance();
+        
         //create the scrapers
         InvestopediaScraper investopediaScraper = new InvestopediaScraper();
         YahooScraper yahooScraper = new YahooScraper();
-        //scrape the data
+        
+        //scrape the summary data
         investopediaScraper.scrapeAllSummaryData();
         yahooScraper.scrapeAllSummaryData();
     }
