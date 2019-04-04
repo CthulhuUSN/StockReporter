6a3f2a19119b4e44e2912fbac06692c612b67402
rajnaidu <l.rajen@hotmail.com>
2019-04-04 09:54:18 -0400

    There was a typo in table name, fixed

--- StockReporter/src/stockreporter/Constants.java
+++ StockReporter/src/stockreporter/Constants.java
@@ -5,7 +5,7 @@ package stockreporter;
  * The program will insert these stocks if the table is empty
  */
 public class Constants {
-    public static final String TABLE_STOCKS = "stocks";
+    public static final String TABLE_STOCK_SOURCE = "STOCK_SOURCE";
     public static final String[] stockSymbols = {"MSFT", "AAPL", "GOOG", "BA", "NFLX", "AMZN", "FB", "CSCO", "TSLA", "TIF"};
     public static final String[] stockNames = {"Microsoft Corporation.", "Apple Inc.", "Alphabet Inc.", "The Boeing Company", "Netflix Inc.", "Amazon.com Inc.", "Facebook Inc.", "Cisco Systems Inc.", "Tesla Inc.", "Tiffany & Co."};
 }
