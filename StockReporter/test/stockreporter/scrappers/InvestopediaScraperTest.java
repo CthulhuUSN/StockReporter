50303c38992831bc6a1697e5514b534b3cdd3f16
jasonarvidson <jasonarvidson@jasons-mbp.domain>
2019-04-04 19:15:27 -0500

    Added the Test html pages to test the scrapers.

--- StockReporter/test/stockreporter/scrappers/InvestopediaScraperTest.java
+++ StockReporter/test/stockreporter/scrappers/InvestopediaScraperTest.java
@@ -1,11 +1,16 @@
 package stockreporter.scrappers;
 
+import java.io.File;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class InvestopediaScraperTest {
     
     public InvestopediaScraperTest() {
+        File input = new File("Apr_4_2019_AAPL_Investopedia.html");
+        Document doc = Jsoup.parse(input, "UTF-8", "");
     }
 
     @Test
