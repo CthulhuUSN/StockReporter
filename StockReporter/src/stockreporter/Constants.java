a5a41c26596c892ee9aaee39be4dfc3dbac55fea
rajnaidu <l.rajen@hotmail.com>
2019-04-06 23:05:01 -0400

    Table name constants

--- StockReporter/src/stockreporter/Constants.java
+++ StockReporter/src/stockreporter/Constants.java
@@ -7,6 +7,8 @@ package stockreporter;
 public class Constants {
     public static final String TABLE_STOCK_SOURCE = "STOCK_SOURCE";
     public static final String TABLE_STOCK_TICKER = "STOCK_TICKER";
+    public static final String TABLE_STOCK_DATE_MAP = "STOCK_DATE_MAP";
+    public static final String TABLE_STOCK_SUMMARY = "STOCK_SUMMARY";
     
     public static final String[] stockSymbols = {"MSFT", "AAPL", "GOOG", "BA", "NFLX", "AMZN", "FB", "CSCO", "TSLA", "TIF"};
     public static final String[] stockNames = {"Microsoft Corporation.", "Apple Inc.", "Alphabet Inc.", "The Boeing Company", "Netflix Inc.", "Amazon.com Inc.", "Facebook Inc.", "Cisco Systems Inc.", "Tesla Inc.", "Tiffany & Co."};
