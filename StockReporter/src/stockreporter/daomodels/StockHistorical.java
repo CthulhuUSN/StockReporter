f44a4e502704c07fe9fad09bd9157feeca4ce0a9
jasonarvidson <jasonarvidson@jasons-mbp.domain>
2019-04-11 08:27:09 -0500

    Added the fields and setters and getters for the stock historical class.

--- StockReporter/src/stockreporter/daomodels/StockHistorical.java
+++ StockReporter/src/stockreporter/daomodels/StockHistorical.java
@@ -1,12 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stockreporter.daomodels;
 
-/**
- * Entity object for stock historical data
- * TODO in next phase of the project
- */
-public class StockHistorical {}
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class StockHistorical implements Serializable {
+    
+    private long historicalId;
+    
+    private BigDecimal open;
+    
+    private BigDecimal high;
+    
+    private BigDecimal low;
+    
+    private BigDecimal close;
+    
+    private BigDecimal adjClose;
+    
+    private long volume;
+
+    public long getHistoricalId() {
+        return historicalId;
+    }
+
+    public void setHistoricalId(long historicalId) {
+        this.historicalId = historicalId;
+    }
+
+    public BigDecimal getOpen() {
+        return open;
+    }
+
+    public void setOpen(BigDecimal open) {
+        this.open = open;
+    }
+
+    public BigDecimal getHigh() {
+        return high;
+    }
+
+    public void setHigh(BigDecimal high) {
+        this.high = high;
+    }
+
+    public BigDecimal getLow() {
+        return low;
+    }
+
+    public void setLow(BigDecimal low) {
+        this.low = low;
+    }
+
+    public BigDecimal getClose() {
+        return close;
+    }
+
+    public void setClose(BigDecimal close) {
+        this.close = close;
+    }
+
+    public BigDecimal getAdjClose() {
+        return adjClose;
+    }
+
+    public void setAdjClose(BigDecimal adjClose) {
+        this.adjClose = adjClose;
+    }
+
+    public long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(long volume) {
+        this.volume = volume;
+    }
+    
+    @Override
+	public String toString() {
+		return "StockHistorical [historicalId=" + historicalId + ", open=" + open + ", high"
+				+ high + ", low=" + low + ", close=" + close + ", adjClose=" + adjClose
+				+ ", volume=" + volume  + "]";
+	}
+}
