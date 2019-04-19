b05f0fc4202de2f61275cc1039093ca7b85eca3c
CthulhuUSN <8527243+CthulhuUSN@users.noreply.github.com>
2019-04-19 15:33:13 -0500

    Update YahooScraperTest.java

--- StockReporter/test/stockreporter/scrappers/YahooScraperTest.java
+++ StockReporter/test/stockreporter/scrappers/YahooScraperTest.java
@@ -17,15 +17,12 @@ import stockreporter.Utility;
 import stockreporter.daomodels.StockTicker;
 import stockreporter.daomodels.StockSummary;
 import stockreporter.daomodels.StockHistorical;
-/**
- *
- * @author klacayo
- */
+
 public class YahooScraperTest {
     StockDao dao;
     
     StockTicker stockTicker = new StockTicker();
-    StockSummary master = new StockSummary();
+    StockSummary ss = new StockSummary();
     StockHistorical sh = new StockHistorical();
     
     public YahooScraperTest() {
@@ -57,25 +54,33 @@ public class YahooScraperTest {
             stockTicker.setName("Apple Inc.");
             stockTicker.setSymbol("AAPL");
             
-            master.setPrevClosePrice(Utility.convertStringCurrency("195.35"));
-            master.setBetaCoefficient(Utility.convertStringCurrency("0.99"));
-            master.setDaysRangeMax(Utility.convertStringCurrency("196.36"));
-            master.setDaysRangeMin(Utility.convertStringCurrency("194.71"));
-            master.setDividentYield(Utility.convertStringCurrency("2.92/1.49%"));
-            master.setEps(Utility.convertStringCurrency("12.12"));
-            master.setFiftyTwoWeeksMax(Utility.convertStringCurrency("233.47"));
-            master.setFiftyTwoWeeksMin(Utility.convertStringCurrency("142.00"));
-            master.setMarketCap(Utility.convertStringCurrency("922.733B"));
-            master.setOpenPrice(Utility.convertStringCurrency("194.79"));
-            master.setPeRatio(Utility.convertStringCurrency("16.14"));
-            master.setSummaryId(0);
-            master.setVolume(Utility.convertStringCurrency("18,747,318.00").longValue());
-            master.setBidPrice(Utility.convertStringCurrency("195.43"));
-            master.setAskPrice(Utility.convertStringCurrency("195.56"));
-            master.setAvgVolume(30598950);
-            master.setEarningDate("Apr 30, 2019");
-            master.setExDividentDate("2019-02-08");
-            master.setOneYearTargetEst(Utility.convertStringCurrency("190.94"));
+            ss.setPrevClosePrice(Utility.convertStringCurrency("195.35"));
+            ss.setBetaCoefficient(Utility.convertStringCurrency("0.99"));
+            ss.setDaysRangeMax(Utility.convertStringCurrency("196.36"));
+            ss.setDaysRangeMin(Utility.convertStringCurrency("194.71"));
+            ss.setDividentYield(Utility.convertStringCurrency("2.92/1.49%"));
+            ss.setEps(Utility.convertStringCurrency("12.12"));
+            ss.setFiftyTwoWeeksMax(Utility.convertStringCurrency("233.47"));
+            ss.setFiftyTwoWeeksMin(Utility.convertStringCurrency("142.00"));
+            ss.setMarketCap(Utility.convertStringCurrency("922.733B"));
+            ss.setOpenPrice(Utility.convertStringCurrency("194.79"));
+            ss.setPeRatio(Utility.convertStringCurrency("16.14"));
+            ss.setSummaryId(0);
+            ss.setVolume(Utility.convertStringCurrency("18,747,318.00").longValue());
+            ss.setBidPrice(Utility.convertStringCurrency("195.43"));
+            ss.setAskPrice(Utility.convertStringCurrency("195.56"));
+            ss.setAvgVolume(30598950);
+            ss.setEarningDate("Apr 30, 2019");
+            ss.setExDividentDate("2019-02-08");
+            ss.setOneYearTargetEst(Utility.convertStringCurrency("190.94"));
+
+            //The information for the date of Nov 26 2018 - When this is tested against what should be the last day scraped.
+            sh.setOpen(Utility.convertStringCurrency("174.24"));
+            sh.setHigh(Utility.convertStringCurrency("174.95"));
+            sh.setLow(Utility.convertStringCurrency("170.26"));
+            sh.setClose(Utility.convertStringCurrency("174.62"));
+            sh.setAdjClose(Utility.convertStringCurrency("173.87"));
+            sh.setVolume(44998500);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -91,33 +96,33 @@ public class YahooScraperTest {
                 try{
             File input = new File("Apr_4_2019_AAPL_Yahoo.html");
             Document tmpDocument = Jsoup.parse(input, "UTF-8", "");
-            
+
             YahooScraper ys = new YahooScraper();
             Class isClass = ys.getClass();
 
             Field f1 = isClass.getDeclaredField("test");
             f1.setAccessible(true);
             f1.set(ys, true);
-            
+
             Field f2 = isClass.getDeclaredField("document");
             f2.setAccessible(true);
             f2.set(ys, tmpDocument);
-            
+
             ys.scrapeSingleSummaryData(stockTicker);
-            
+
             Field f3 = isClass.getDeclaredField("summaryData");
             f3.setAccessible(true);
             StockSummary results = (StockSummary)f3.get(ys);
             //This isn't scraped so don't care about the vaule since it is a DB thing.
-            master.setStockDtMapId(results.getStockDtMapId());
-            
-            String expected = master.toString();
+            ss.setStockDtMapId(results.getStockDtMapId());
+
+            String expected = ss.toString();
             String actual = results.toString();
             System.out.println("Expected:" + expected);
             System.out.println("Actual:  " + actual);
-            
+
             assertEquals(expected, actual);
-            
+
             //truncate the data after test
             dao.getInstance();
             dao.deleteAll();
@@ -127,6 +132,49 @@ public class YahooScraperTest {
         }
         
     }
+
+    @Test
+    public void testscrapeSingleHisotricalData() {
+        try {
+            File input = new File("Apr_19_2019_AAPL_Historical_Yahoo.html");
+            Document tmpDocument = Jsoup.parse(input, "UTF-8", "");
+
+            YahooScraper ys = new YahooScraper();
+            Class isClass = ys.getClass();
+
+            Field f1 = isClass.getDeclaredField("test");
+            f1.setAccessible(true);
+            f1.set(ys, true);
+
+            Field f2 = isClass.getDeclaredField("document");
+            f2.setAccessible(true);
+            f2.set(ys, tmpDocument);
+
+            ys.scrapeSingleHistoricalData(stockTicker);
+
+            Field f3 = isClass.getDeclaredField("stockHistorical");
+            f3.setAccessible(true);
+            StockHistorical results = (StockHistorical)f3.get(ys);
+            //This isn't scraped so don't care about the vaule since it is a DB thing.
+            sh.setStockDtMapId(results.getStockDtMapId());
+            sh.setHistoricalId(results.getHistoricalId());
+
+            String expected = sh.toString();
+            String actual = results.toString();
+            System.out.println("Expected:" + expected);
+            System.out.println("Actual:  " + actual);
+
+            assertEquals(expected, actual);
+
+            //truncate the data after test
+            dao.getInstance();
+            dao.deleteAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
     @Test
     public void testscrapeSingleHisotricalData() {
         try {
@@ -144,6 +192,7 @@ public class YahooScraperTest {
             e.printStackTrace();
         }
     }
+     */
     
     
 }
